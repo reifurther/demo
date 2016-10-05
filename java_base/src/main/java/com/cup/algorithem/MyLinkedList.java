30eed6c51356a6bbd586bf9d5ac294e2fc4e2b10
reifurther <leipingr@gmail.com>
2016-10-05 20:55:05 +0800

    add linklist case

--- java_base/src/main/java/com/cup/algorithem/MyLinkedList.java
+++ java_base/src/main/java/com/cup/algorithem/MyLinkedList.java
@@ -1,6 +1,8 @@
 package com.cup.algorithem;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * Created by reifurther on 16/10/3.
@@ -8,9 +10,20 @@ import java.util.Iterator;
 public class MyLinkedList<AnyType> implements Iterable<AnyType> {
 
     private int theSize;
+    private int modCount = 0;
+    private Node<AnyType> beginMarker;
+    private Node<AnyType> endMarker;
 
     private static class Node<AnyType>{
+        private AnyType data;
+        private Node<AnyType> prev;
+        private Node<AnyType> next;
 
+        public Node(AnyType d, Node<AnyType> p, Node<AnyType> n){
+            data = d;
+            prev = p;
+            next = n;
+        }
     }
 
     public MyLinkedList(){
@@ -18,6 +31,12 @@ public class MyLinkedList<AnyType> implements Iterable<AnyType> {
     }
 
     private void doClear() {
+        beginMarker = new Node<AnyType>(null, null, null);
+        endMarker = new Node<AnyType>(null, beginMarker, null);
+        beginMarker.next = endMarker;
+
+        theSize = 0;
+        modCount++;
     }
 
     public void clear(){
@@ -38,13 +57,120 @@ public class MyLinkedList<AnyType> implements Iterable<AnyType> {
     }
 
     public void add(int idx, AnyType x){
-
+        addBefore(getNode(idx, 0, size()), x);
     }
 
+    public void addBefore(Node<AnyType> p, AnyType x){
+        Node<AnyType> newNode = new Node<>(x, p.next, p);
+        newNode.prev.next = newNode;
+        p.prev = newNode;
+
+        theSize++;
+        modCount++;
+    }
+
+    public AnyType get(int idx){
+        return getNode(idx).data;
+    }
+
+    public AnyType set(int idx, AnyType newVal){
+        Node<AnyType> p = getNode(idx);
+        AnyType oldVal = p.data;
+        p.data = newVal;
+        return oldVal;
+    }
+
+    public AnyType remove(int idx){
+        return remove(getNode(idx));
+    }
+
+    public AnyType remove(Node<AnyType> p){
+        p.next.prev = p.prev;
+        p.prev.next = p.next;
+
+        theSize--;
+        modCount++;
+
+        return p.data;
+    }
+
+    private Node<AnyType> getNode(int idx){
+        return getNode(idx, 0, size()-1);
+    }
+
+    private Node<AnyType> getNode(int idx, int lower, int upper){
+
+        Node<AnyType> p ;
+
+        if(idx < lower || idx > upper){
+            throw new IndexOutOfBoundsException();
+        }
+
+        if(idx < size()/2){
+            p = beginMarker.next;
+            for(int i=0; i<idx; i++){
+                p = p.next;
+            }
+        }else{
+            p = endMarker;
+            for(int i=size(); i>idx; i--){
+                p = p.prev;
+            }
+        }
+
+        return p;
+    }
 
 
     @Override
     public Iterator<AnyType> iterator() {
-        return null;
+        return new LinkedListIterator();
+    }
+
+    private class LinkedListIterator implements Iterator<AnyType>{
+
+        private Node<AnyType> current = beginMarker.next;
+        private int expectModCount = modCount;
+        private boolean okToRemove = false;
+
+        @Override
+        public boolean hasNext() {
+            return current != endMarker;
+        }
+
+        @Override
+        public AnyType next() {
+
+            if(modCount != expectModCount){
+                throw new ConcurrentModificationException();
+            }
+
+            if(!hasNext()){
+                throw  new NoSuchElementException();
+            }
+
+            AnyType nextItem = current.data;
+            current = current.next;
+            okToRemove = true;
+
+            return nextItem;
+        }
+
+        @Override
+        public void remove() {
+
+            if(modCount != expectModCount){
+                throw new ConcurrentModificationException();
+            }
+
+            if(!okToRemove){
+                throw new IllegalStateException();
+            }
+
+            MyLinkedList.this.remove(current.prev);
+            expectModCount++;
+            okToRemove = false;
+
+        }
     }
 }
---
+++ java_base/src/main/java/com/cup/algorithem/MyQueue.java
@@ -0,0 +1,80 @@
+package com.cup.algorithem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by reifurther on 16/10/5.
+ */
+public class MyQueue {
+
+    public int front;
+    public int back;
+
+    public int currentSize = 0;
+
+    public int[] myList;
+
+    public MyQueue(int[] list){
+        myList = list;
+    }
+
+    public int getFront(int[] x){
+
+        if(x==null){
+            throw new NoSuchElementException();
+        }
+
+        this.myList = x;
+
+        return front;
+    }
+
+    public int getBack(int[] x){
+
+        return back;
+    }
+
+    public int getCurrentSize(int[] x){
+        return x.length;
+    }
+
+
+
+    //use int
+    public void enQueue(int x){
+
+        if(front < 0 || back < 0){
+            throw new NoSuchElementException();
+        }
+
+        back++;
+        currentSize++;
+
+        myList[back] = x;
+    }
+
+    public int deQueue(int front){
+        front++;
+        back--;
+        currentSize--;
+        return myList[front];
+    }
+
+    public static void main(String[] args) {
+
+        int[] testInts = {1,2,3,4,5};
+        MyQueue queue = new MyQueue(testInts);
+
+        queue.enQueue(6);
+
+        for(int i=0; i<testInts.length; i++){
+            System.out.println("Int Array list: " + testInts[i]);
+        }
+
+
+
+
+    }
+}
